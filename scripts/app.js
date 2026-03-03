6d1b57eb579f9b327fce045475ab3e4100f27d0b
Alexander <alexanderyeh5270@gmail.com>
2026-03-03 13:05:49 -0500

    Added POST request routing + survey results printed to console when submitted

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,9 @@ var app = express();
 // set up template engine
 app.set("view engine", "ejs");
 
+// needed to process data from HTML form POST req
+app.use(express.urlencoded({ extended: true }));
+
 // static file serving
 app.use(express.static("./public"));
 
--- scripts/server.js
+++ scripts/server.js
@@ -6,6 +6,10 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname, "../views/survey.html"));
   });
 
+  app.post("/survey", function (req, res) {
+    res.render("results", { responses: req.body });
+    console.log("Received survey responses:", req.body);
+  });
   // Analyst view (using EJS)
   app.get("/results", function (req, res) {
     res.render("results");
